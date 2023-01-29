ebce17378ff8dcbb4f36238e1f9dcef75349a67f
Syed-sulaimaan <92155608+Syed-sulaimaan@users.noreply.github.com>
2023-01-29 13:12:17 +0530

    feat: Added bg and name

---
+++ src/components/HomePage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="h-screen bg-[url('src/assets/bg.png')] bg-bottom bg-origin-padding">
+        <h1><img class="h-[450px] w-full" src="src/assets/ezaan.svg" /></h1>
+    </div>
+</template>
