9902805ded0aad0dad0883cabe3beb4b3b952bc5
Syed-sulaimaan <92155608+Syed-sulaimaan@users.noreply.github.com>
2023-01-29 13:48:40 +0530

    feat: Added footer social icons

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="h-screen bg-[url('src/assets/bg.png')] bg-bottom bg-origin-padding">
+    <div class="flex flex-col items-center h-screen bg-[url('src/assets/bg.png')] bg-bottom bg-origin-padding">
         <h1><img class="h-[450px] w-full" src="src/assets/ezaan.svg" /></h1>
+        <div class="flex justify-evenly mt-20 w-80">
+            <a target="_new" href="https://instagram.com/syed.ezaan">
+                <v-icon name="ri-linkedin-line" scale="1.5" class="text-white" />
+            </a>
+            <a target="_new" href="https://instagram.com/syed.ezaan">
+                <v-icon name="ri-instagram-line" scale="1.5" class="text-white" />
+            </a>
+            <a target="_new" href="https://instagram.com/syed.ezaan">
+                <v-icon name="ri-twitter-line" scale="1.5" class="text-white" />
+            </a>
+        </div>
+        <div class="fixed bottom-0 mb-4 text-gray-500 text-xs text-center">
+            Copyright &copy; 2023. Syed Ezaan<br>
+            Developed and Designed by Sysu
+        </div>
     </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { RiLinkedinLine, RiInstagramLine, RiTwitterLine } from "oh-vue-icons/icons";
+
 import "./assets/main.css";
+
+addIcons(RiLinkedinLine, RiInstagramLine, RiTwitterLine);
 
 const app = createApp(App);
 
+app.component("v-icon", OhVueIcon);
 app.mount("#main");
