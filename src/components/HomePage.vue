9b4a3cdfb943412a099f3af85b568c515e25f894
Syed-sulaimaan <92155608+Syed-sulaimaan@users.noreply.github.com>
2023-01-30 17:44:31 +0530

    added svg icons, added theme support, added mobile version support

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="flex flex-col items-center h-screen bg-[url('src/assets/bg.png')] bg-bottom bg-origin-padding">
         <h1><img class="h-[640px] sm:h-[450px] w-full" src="src/assets/ezaan.svg" /></h1>
-        <div class="flex justify-evenly mt-20 sm:mt-20 w-80 ">
-            <a target="_new" href="https://instagram.com/syed.ezaan">
-                <v-icon name="ri-linkedin-line" scale="1.5" class="text-white hover:[filter:drop-shadow(0px_0px_215.46px_#AC1DBB)_drop-shadow(0px_0px_123.12px_#AC1DBB)_drop-shadow(0px_0px_71.82px_#AC1DBB)_drop-shadow(0px_0px_35.91px_#AC1DBB)_drop-shadow(0px_0px_10.26px_#AC1DBB)_drop-shadow(0px_0px_5.13px_#AC1DBB)] transition transition-shadow ease-in duration-1000" />
+        <div class="absolute flex justify-evenly bottom-1 mb-20 sm:mt-20 w-80 ">
+            <a target="_new" href="https://www.linkedin.com/in/syedezaaan">
+                <img class="w-6 hover:[filter:drop-shadow(0px_0px_215.46px_#AC1DBB)_drop-shadow(0px_0px_123.12px_#AC1DBB)_drop-shadow(0px_0px_71.82px_#AC1DBB)_drop-shadow(0px_0px_35.91px_#AC1DBB)_drop-shadow(0px_0px_10.26px_#AC1DBB)_drop-shadow(0px_0px_5.13px_#AC1DBB)] " src="src/assets/Outline/linkedin.svg" />
             </a>
-            <a target="_new" href="https://instagram.com/syed.ezaan">
-                <v-icon name="ri-instagram-line" scale="1.5" class="text-white hover:[filter:drop-shadow(0px_0px_215.46px_#AC1DBB)_drop-shadow(0px_0px_123.12px_#AC1DBB)_drop-shadow(0px_0px_71.82px_#AC1DBB)_drop-shadow(0px_0px_35.91px_#AC1DBB)_drop-shadow(0px_0px_10.26px_#AC1DBB)_drop-shadow(0px_0px_5.13px_#AC1DBB)] transition transition-shadow ease-in duration-1000" />
+            <a target="_new" href="https://www.instagram.com/syedezaaan/">
+                <v-icon name="ri-instagram-line" scale="1.5" class="text-white hover:[filter:drop-shadow(0px_0px_215.46px_#AC1DBB)_drop-shadow(0px_0px_123.12px_#AC1DBB)_drop-shadow(0px_0px_71.82px_#AC1DBB)_drop-shadow(0px_0px_35.91px_#AC1DBB)_drop-shadow(0px_0px_10.26px_#AC1DBB)_drop-shadow(0px_0px_5.13px_#AC1DBB)] " />
             </a>
-            <a target="_new" href="https://instagram.com/syed.ezaan">
-                <v-icon name="ri-twitter-line" scale="1.5" class="text-white hover:[filter:drop-shadow(0px_0px_215.46px_#AC1DBB)_drop-shadow(0px_0px_123.12px_#AC1DBB)_drop-shadow(0px_0px_71.82px_#AC1DBB)_drop-shadow(0px_0px_35.91px_#AC1DBB)_drop-shadow(0px_0px_10.26px_#AC1DBB)_drop-shadow(0px_0px_5.13px_#AC1DBB)] transition transition-shadow ease-in duration-1000" />
+            <a target="_new" href="https://twitter.com/syedezaaan">
+                <v-icon name="ri-twitter-line" scale="1.5" class="text-white hover:[filter:drop-shadow(0px_0px_215.46px_#AC1DBB)_drop-shadow(0px_0px_123.12px_#AC1DBB)_drop-shadow(0px_0px_71.82px_#AC1DBB)_drop-shadow(0px_0px_35.91px_#AC1DBB)_drop-shadow(0px_0px_10.26px_#AC1DBB)_drop-shadow(0px_0px_5.13px_#AC1DBB)] transition-shadow ease-in-out duration-3000" />
             </a>
         </div>
         <div class="fixed bottom-0 mb-4 text-gray-500 text-xs text-center">
