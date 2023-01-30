8716b70d56614854f8317edf7fb70705adffedaf
Syed-sulaimaan <92155608+Syed-sulaimaan@users.noreply.github.com>
2023-01-30 22:45:57 +0530

    fixed relative url issue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="flex flex-col items-center h-screen bg-[url('src/assets/bg.png')] bg-bottom bg-origin-padding"
+    class="flex flex-col items-center h-screen bg-[url('../assets/bg.png')] bg-bottom bg-origin-padding"
   >
     <h1>
-      <img class="h-[640px] sm:h-[450px] w-full" src="/src/assets/ezaan.svg" />
+      <img class="h-[640px] sm:h-[450px] w-full" src="../assets/ezaan.svg" />
     </h1>
     <div class="absolute flex justify-evenly bottom-1 mb-20 sm:mt-20 w-80">
       <a target="_new" href="https://www.linkedin.com/in/syedezaaan">
         <img
           class="w-6 hover:[filter:drop-shadow(0px_0px_215.46px_#AC1DBB)_drop-shadow(0px_0px_123.12px_#AC1DBB)_drop-shadow(0px_0px_71.82px_#AC1DBB)_drop-shadow(0px_0px_35.91px_#AC1DBB)_drop-shadow(0px_0px_10.26px_#AC1DBB)_drop-shadow(0px_0px_5.13px_#AC1DBB)]"
-          src="/src/assets/Outline/linkedin.svg"
+          src="../assets/Outline/linkedin.svg"
         />
       </a>
       <a target="_new" href="https://www.instagram.com/syedezaaan/">
